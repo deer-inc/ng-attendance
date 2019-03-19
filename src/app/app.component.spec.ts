b1b44ee0ca05d194b43dd626eca3f5a223dab568
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-03-19 15:32:44 +0900

    fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,17 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'ng-attendance-manager'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-attendance-manager');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-attendance-manager!');
-  });
 });
